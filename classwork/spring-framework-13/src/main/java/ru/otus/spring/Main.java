ddfd19128adf048337158d57e12758358ab24609
salasky <65674987+salasky@users.noreply.github.com>
2022-04-29 21:17:34 +0500

    Exec 1

---
+++ classwork/spring-framework-13/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,30 @@
+package ru.otus.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.map.repository.config.EnableMapRepositories;
+import ru.otus.spring.domain.Person;
+import ru.otus.spring.repostory.PersonRepository;
+
+import javax.annotation.PostConstruct;
+
+@EnableMapRepositories
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    @Autowired
+    private PersonRepository repository;
+
+    @PostConstruct
+    public void init() {
+        repository.save(new Person(0, "Pushkin"));
+        repository.save(new Person(1, "Lermantov"));
+        repository.save(new Person(2, "Gogol"));
+    }
+}
