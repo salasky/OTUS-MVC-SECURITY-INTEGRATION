6b6c26390af97423061a5c51c894ec07c72325d6
salasky <65674987+salasky@users.noreply.github.com>
2022-05-10 13:54:25 +0500

    Занятие 18. Spring security

--- classwork/spring-framework-16/src/main/java/ru/otus/Main.java
+++ classwork/spring-framework-16/src/main/java/ru/otus/Main.java
@@ -3,12 +3,26 @@ package ru.otus;
 
 import io.reactivex.Observable;
 
+import io.reactivex.disposables.Disposable;
+
+import java.io.IOException;
+
+
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class Main {
 
-    public static void main(String[] args) {
-        Observable.range(20, 5)
-                // TODO: add code here
-                .subscribe(System.out::println);
+    public static void main(String[] args) throws IOException {
+/*        Observable<Long> values=Observable.interval(1/1000000000, TimeUnit.NANOSECONDS);
+         Disposable subscription=values.subscribe(
+                v->System.out.println("Recived:"+v),
+                e-> System.out.println("Error:"+e),
+                ()-> System.out.println("Complited")
+        );
+        System.in.read();*/
+
+
+        Observable<Integer> s=Observable.range(20, 5);
+        Observable<Integer> d=Observable.range(10, 5);
+
     }
 }
