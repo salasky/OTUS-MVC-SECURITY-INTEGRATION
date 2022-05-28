1c38e78201084e15d8adeb235508f2a936f94c21
salasky <65674987+salasky@users.noreply.github.com>
2022-05-28 21:51:03 +0500

    Hateoas,actuator.Микросервисы.Упр1

---
+++ classwork/spring-framework-27/src/main/java/ru/otus/spring/microservice/config/HealthIndicator.java
@@ -0,0 +1,27 @@
+package ru.otus.spring.microservice.config;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class HealthIndicator implements org.springframework.boot.actuate.health.HealthIndicator {
+
+    @Override
+    public Health health() {
+       while (true) {
+           try {
+               Thread.sleep(5000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+           int i= new Random().nextInt(10);
+           if(i%2==0) {
+               return Health.down().build();
+           }
+           else
+               return Health.up().build();
+       }
+    }
+}
